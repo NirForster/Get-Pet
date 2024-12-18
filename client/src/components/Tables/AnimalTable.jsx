9d23c60e66bc973922a1d8ef872d8af20636902e
yoavi1 <yoavmeirovitz187@gmail.com>
2024-12-18 17:45:06 +0200

    commit 3

--- client/src/components/Tables/AnimalTable.jsx
+++ client/src/components/Tables/AnimalTable.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '../../components/ui/tabs.tsx';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../../components/ui/table';
+
+const dummyData = [
+  {
+    _id: '1',
+    name: 'rex',
+    status: 'is still waiting for you.',
+    imgUrl: dogSvg,
+  },
+  {
+    _id: '2',
+    name: 'baba',
+    status: 'is still waiting for you.',
+    imgUrl: dogSvg,
+  },
+  {
+    _id: '3',
+    name: 'yuval',
+    status: 'is still waiting for you.',
+    imgUrl: dogSvg,
+  },
+];
+
+import dogSvg from '/dog-svgrepo-com.svg';
 export default function AnimalTable() {
-  const [animalName, setAnimalName] = useState('rex');
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    // fetch data from backend
+    setAnimals(dummyData);
+  }, []);
 
   return (
     <TabsContent className="text-center" value="account">
@@ -14,13 +57,19 @@ export default function AnimalTable() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">{animalName}</TableCell>
-            <TableCell>{animalName} is still waiting for you.</TableCell>
-            <TableCell>
-              <img src={dogSvg}></img>
-            </TableCell>
-          </TableRow>
+          {animals.map((animal) => {
+            return (
+              <TableRow key={animal._id}>
+                <TableCell className="font-medium">{animal.name}</TableCell>
+                <TableCell>
+                  {animal.name} {animal.status}
+                </TableCell>
+                <TableCell>
+                  <img src={animal.imgUrl}></img>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </TabsContent>
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -24,8 +24,8 @@ import {
   AvatarImage,
 } from '../../components/ui/avatar';
 export default function Profile() {
-  //   const [animalName, setAnimalName] = useState('rex');
-  //   const [count, setCount] = useState('0');
+  const [animalName, setAnimalName] = useState('rex');
+  const [count, setCount] = useState('0');
   return (
     <div>
       <Avatar>
@@ -37,28 +37,9 @@ export default function Profile() {
           <TabsTrigger value="account">Animals</TabsTrigger>
           <TabsTrigger value="password">siters</TabsTrigger>
         </TabsList>
-        <animalName />
-        <AnimalTable></AnimalTable>
-        {/* <TabsContent className="text-center" value="account">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">name</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>image</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">{animalName}</TableCell>
-                <TableCell>{animalName} is still waiting for you.</TableCell>
-                <TableCell>
-                  <img src={dogSvg}></img>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TabsContent> */}
+
+        <AnimalTable />
+
         <TabsContent value="password">Change your password here.</TabsContent>
       </Tabs>
     </div>
