94185dc739cc80939c306797165702ff445aa16a
yosefhayim <yosefisabag+03@gmail.com>
2024-12-18 22:17:51 +0200

    set redux default user profile img

--- client/src/components/TopNavbar/TopNavbar.tsx
+++ client/src/components/TopNavbar/TopNavbar.tsx
@@ -2,14 +2,13 @@ import { FaBell } from "react-icons/fa";
 import { IoLocationSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
-import { useDispatch } from "react-redux";
-import { setProfilePicUser } from "@/store/slices/userSlice.ts";
+import { useSelector } from "react-redux";
 
 const iconStyle = "p-[0.5em]";
 
 const TopNavbar = () => {
-  const dispatch = useDispatch();
-  // dispatch(setProfilePicUser(''));
+  const setDefaultImg = useSelector((state) => state.user.profileImg);
+
   return (
     <div className="flex flex-row items-center justify-between absolute top-0 w-full left-0 p-[1em]">
       <div className="flex flex-row gap-[0.3em]">
