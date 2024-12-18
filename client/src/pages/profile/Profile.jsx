e963ea16b42773dbf27a0181051bd0d3425019a9
yoavi1 <yoavmeirovitz187@gmail.com>
2024-12-18 16:24:05 +0200

    commit 3

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import AnimalTable from '../../components/Tables/AnimalTable';
+import { useState } from 'react';
+import dogSvg from '/dog-svgrepo-com.svg';
 import { Button } from '../../components/ui/button.tsx';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../../components/ui/table.tsx';
 import {
   Tabs,
   TabsContent,
@@ -12,23 +24,41 @@ import {
   AvatarImage,
 } from '../../components/ui/avatar';
 export default function Profile() {
+  //   const [animalName, setAnimalName] = useState('rex');
+  //   const [count, setCount] = useState('0');
   return (
     <div>
-      {/* Profile
-      <Tabs className="">kjjkkj</Tabs>
-      <Button>yoav</Button> */}
       <Avatar>
-        <AvatarImage src="/public/avataryoav.svg" alt="@shadcn" />
+        <AvatarImage src="/avataryoav.svg" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
-      <Tabs className="">
+      <Tabs className="w-72">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="account">Animals</TabsTrigger>
           <TabsTrigger value="password">siters</TabsTrigger>
         </TabsList>
-        <TabsContent className="text-center" value="account">
-          ddd
-        </TabsContent>
+        <animalName />
+        <AnimalTable></AnimalTable>
+        {/* <TabsContent className="text-center" value="account">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">name</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>image</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">{animalName}</TableCell>
+                <TableCell>{animalName} is still waiting for you.</TableCell>
+                <TableCell>
+                  <img src={dogSvg}></img>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TabsContent> */}
         <TabsContent value="password">Change your password here.</TabsContent>
       </Tabs>
     </div>
