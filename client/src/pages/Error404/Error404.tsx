c8ca4bbe5bd9d89767f96ab2037ff601d7a530fe
yosefhayim <yosefisabag+03@gmail.com>
2024-12-19 09:47:01 +0200

    rew

--- client/src/pages/Error404/Error404.tsx
+++ client/src/pages/Error404/Error404.tsx
@@ -1,16 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import dogWatchTv from "/error-404.png";
+import fourOfourText from "/404-text.svg";
 
 const Error404 = () => {
   const navigate = useNavigate();
 
-  // setTimeout(() => {
-  //   navigate("/");
-  // }, 3000);
+  setTimeout(() => {
+    navigate("/");
+  }, 4000);
 
   return (
     <div>
-      <p className="text-black text-[3em]">Woof 404 woof!!!</p>
+      <img src={fourOfourText} alt="" className="w-full" />
       <img src={dogWatchTv} alt="" />
     </div>
   );
