305a74cd10f1f0733761429faa514541137c9276
yosefhayim <yosefisabag+03@gmail.com>
2024-12-19 08:18:15 +0200

    Merge branch 'main' of https://github.com/NirForster/Get-Pet

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { IoHeartCircleSharp } from "react-icons/io5";
 import { useSelector } from "react-redux";
 import { isUserAllowed } from "../../utils/isAuth.js";
 import { useNavigate } from "react-router-dom";
+import Loader from "@/components/Loader/Loader.js";
 
 // Define the types for your pet data
 interface PetData {
@@ -35,6 +36,7 @@ const Dashboard: React.FC = () => {
   const [page, setPage] = useState<number>(1);
   const [data, setData] = useState<PetData | null>(null);
   const [petId, setPetId] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const userId = useSelector((state: RootState) => state.user.userId);
 
   const addPetToFavorite = async () => {
@@ -60,6 +62,7 @@ const Dashboard: React.FC = () => {
 
   const fetchPets = async () => {
     try {
+      setLoading(true); // Show loader
       const res = await axios.get<{
         data: PetData[];
       }>(
@@ -73,6 +76,8 @@ const Dashboard: React.FC = () => {
       }
     } catch (error) {
       console.error("Error occurred during fetching pets data:", error);
+    } finally {
+      setLoading(false); // Hide loader
     }
   };
 
@@ -96,42 +101,49 @@ const Dashboard: React.FC = () => {
       className="flex flex-col items-center justify-center gap-4"
       onClick={handleClick}
     >
-      <div>
-        <h1 className="font-titleFont text-[2em] text-center">
-          <span className="text-chosenYellow">Swipe. </span>
-          <span className="text-chosenRed">Love.</span>
-          <span> Adopt.</span>
-        </h1>
-      </div>
-      <div className="rounded-[1em] h-[350px] w-[250px] bg-gray-200 flex items-center justify-center">
-        {data ?
-          <img
-            src={data.images[0]}
-            alt={data.name}
-            className="w-full h-full rounded-[1em] object-cover"
-          />
-        : <p>Loading...</p>}
-      </div>
-      <div className="text-center mt-2">
-        {data && (
-          <>
-            <h2 className="font-bold text-lg">{data.name}</h2>
-            <p className="text-gray-600">
-              {data.age} years old - {data.breed}
-            </p>
-            <p className="text-gray-500">{data.description}</p>
-            <p className="text-gray-400 italic">From: {data.adoptionCenter}</p>
-          </>
-        )}
-      </div>
-      <div className="flex flex-row flex-wrap w-full justify-center gap-[1em] mt-4">
-        <button value="declined">
-          <IoMdCloseCircle className="text-[4em] text-chosenRed hover:text-black" />
-        </button>
-        <button value="next">
-          <IoHeartCircleSharp className="text-[4em] text-green-500 hover:text-black" />
-        </button>
-      </div>
+      {loading ?
+        <Loader />
+      : <>
+          <div>
+            <h1 className="font-titleFont text-[2em] text-center">
+              <span className="text-chosenYellow">Swipe. </span>
+              <span className="text-chosenRed">Love.</span>
+              <span> Adopt.</span>
+            </h1>
+          </div>
+          <div className="rounded-[1em] h-[350px] w-[250px] bg-gray-200 flex items-center justify-center">
+            {data && (
+              <img
+                src={data.images[0]}
+                alt={data.name}
+                className="w-full h-full rounded-[1em] object-cover"
+              />
+            )}
+          </div>
+          <div className="text-center mt-2">
+            {data && (
+              <>
+                <h2 className="font-bold text-lg">{data.name}</h2>
+                <p className="text-gray-600">
+                  {data.age} years old - {data.breed}
+                </p>
+                <p className="text-gray-500">{data.description}</p>
+                <p className="text-gray-400 italic">
+                  From: {data.adoptionCenter}
+                </p>
+              </>
+            )}
+          </div>
+          <div className="flex flex-row flex-wrap w-full justify-center gap-[1em] mt-4">
+            <button value="declined">
+              <IoMdCloseCircle className="text-[4em] text-chosenRed hover:text-black" />
+            </button>
+            <button value="next">
+              <IoHeartCircleSharp className="text-[4em] text-green-500 hover:text-black" />
+            </button>
+          </div>
+        </>
+      }
     </div>
   );
 };
--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -2,6 +2,7 @@ import { Input } from "@/components/ui/input";
 import { FiSearch } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { isUserAllowed } from "../../utils/isAuth.js";
+import PetGrid from "@/components/PetGrid/PetGrid.js";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -14,7 +15,12 @@ const Search = () => {
   return (
     <div className="relative w-full max-w-md">
       <FiSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400" />
-      <Input placeholder="Search for dog-sitter" className="pl-10" />
+      <div className="mt-[5em] p-[1em]">
+        <Input placeholder="Search for dog-sitter" className="pl-10" />
+      </div>
+      <div className="mt-[1em]">
+        <PetGrid />
+      </div>
     </div>
   );
 };
